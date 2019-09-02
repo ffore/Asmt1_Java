b85659358643e9c00e24b64324ce4ac1c59966ef
rook275 <rook275@ufl.edu>
2019-09-02 17:47:33 -0400

    ShortestDistance core finished. Waiting on Precision issue

--- src/ShortestDistance.java
+++ src/ShortestDistance.java
@@ -2,15 +2,29 @@ import java.util.Scanner;
 
 public class ShortestDistance {
 
+    private double distance;
+    private double[] coordinate;
+
+    public ShortestDistance() {
+        this.distance = 0.0;
+        this.coordinate = new double[4];
+    }
+
+    public ShortestDistance(double[] coordinate) {
+        this.distance = 0.0;
+        this.coordinate = coordinate;
+    }
+
     public static void main(String[] args) {
         ShortestDistance shortestDistance = new ShortestDistance();
-        double[] coordinate = new double[4];
         String[] parameter = {"x1", "y1", "x2", "y2"};
 
         for(int i = 0; i < 4; i++) {
-            coordinate[i] = shortestDistance.acceptCoordinate(parameter[i]);
+            shortestDistance.coordinate[i] = shortestDistance.acceptCoordinate(parameter[i]);
         }
 
+        shortestDistance.calculateDistance();
+        System.out.println("The calculated distance is: " + shortestDistance.getDistance());
     }
 
     public static boolean isValidNumber(String input) {
@@ -36,5 +50,49 @@ public class ShortestDistance {
         }
 
         return Double.parseDouble(input);
+    }
+
+    public void calculateDistance() {
+        double xDifference, yDifference, xSquared, ySquared, sum;
+        double[] values = this.getCoordinates();
+
+        xDifference = subtract(values[2], values[0]);
+        yDifference = subtract(values[3], values[1]);
+
+        xSquared = square(xDifference);
+        ySquared = square(yDifference);
+
+        sum = add(xSquared, ySquared);
+
+        // Math Library being used for square root ONLY.
+        this.setDistance(Math.sqrt(sum));
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double square(double a) {
+        return a * a;
+    }
+
+    public double getDistance() {
+        return this.distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public double[] getCoordinates() {
+        return this.coordinate;
+    }
+
+    public void setCoordinates(double[] coordinate) {
+        this.coordinate = coordinate;
     }
 }
--- test/ShortestDistanceTest.java
+++ test/ShortestDistanceTest.java
@@ -1,11 +1,5 @@
 import org.junit.jupiter.api.Test;
-
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.InputStream;
 import java.io.StringBufferInputStream;
-import java.util.Scanner;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ShortestDistanceTest {
@@ -49,6 +43,139 @@ public class ShortestDistanceTest {
         StringBufferInputStream inputStream = new StringBufferInputStream("20");
         System.setIn(inputStream);
         assertEquals(shortestDistance.acceptCoordinate("x1"), 20);
+    }
+
+    // Subtraction
+    @Test
+    public void testSubtractionWithPositiveNumbers() {
+        assertEquals(shortestDistance.subtract(5.0, 3.0), 2.0);
+    }
+
+    @Test
+    public void testSubtractionWithNegativeNumbers() {
+        assertEquals(shortestDistance.subtract(-3, -2), -1);
+    }
+
+    @Test
+    public void testSubtractionWithOppositeSignedNumbers() {
+        assertEquals(shortestDistance.subtract(-3, 3), -6);
+        assertEquals(shortestDistance.subtract(3, -3), 6);
+    }
+
+    @Test
+    public void testSubtractionWithDecimalNumbers() {
+        assertEquals(shortestDistance.subtract(-2.5, -2.0), -0.5);
+    }
+
+//    @Test
+//    public void testSubtractionWithSixDigitDecimalNumbers() {
+//        assertEquals(shortestDistance.subtract(5.666666, 4.333333), 1.333333);
+//        System.out.println(shortestDistance.subtract(4.4, 4.2));
+//    }
+
+    // Addition
+    @Test
+    public void testAdditionWithPositiveNumbers() {
+        assertEquals(shortestDistance.add(1.0, 1.0), 2.0);
+    }
+
+    @Test
+    public void testAdditionwithNegativeNumbers() {
+        assertEquals(shortestDistance.add(-1, -11), -12);
+    }
+
+    @Test
+    public void testAdditionWithOppositeSignedNumbers() {
+        assertEquals(shortestDistance.add(1, -1), 0);
+    }
+
+    @Test
+    public void testAdditionWithDecimalNumbers() {
+        assertEquals(shortestDistance.add(1.5, 3.5), 5);
+    }
+
+    @Test
+    public void testAdditionWithSixDigitDecimalNumbers() {
+        assertEquals(shortestDistance.add(1.111111, 3.888888), 4.999999);
+    }
+
+    // Square
+    @Test
+    public void testSquareWithPositiveNumber() {
+        assertEquals(shortestDistance.square(5.0), 25);
+    }
+
+    @Test
+    public void testSquareWithNegativeNumber() {
+        assertEquals(shortestDistance.square(-5), 25);
+    }
+
+    @Test
+    public void testSquareWithDecimalNumber() {
+        assertEquals(shortestDistance.square(2.5), 6.25);
+    }
+
+//    @Test
+//    public void testSquareWithSixDigitDecimalNumber() {
+//        assertEquals(shortestDistance.square(2.112233), 4.46152825);
+//    }
+
+    // Private Variable Interactions
+    @Test
+    public void testGetDistance() {
+        ShortestDistance emptyDistance = new ShortestDistance();
+        assertEquals(emptyDistance.getDistance(), 0);
+    }
+
+    @Test
+    public void testSetDistance() {
+        ShortestDistance emptyDistance = new ShortestDistance();
+        emptyDistance.setDistance(2);
+        assertEquals(emptyDistance.getDistance(), 2);
+
+    }
+
+    @Test
+    public void testGetCoordinate() {
+        ShortestDistance emptyCoordinate = new ShortestDistance();
+        double[] arr = emptyCoordinate.getCoordinates();
+        assertEquals(arr.length, 4);
+    }
+
+    @Test
+    public void testSetCoordinate() {
+        ShortestDistance emptyCoordinate = new ShortestDistance();
+        double[] arr = {1, 2, 3, 4};
+        emptyCoordinate.setCoordinates(arr);
+        assertEquals(emptyCoordinate.getCoordinates(), arr);
+    }
+
+    // Distance
+    @Test
+    public void testDistanceCalculationWithWholePositiveNumbers() {
+        double[] values = {1, 1, 2, 2};
+        ShortestDistance shortestDistance = new ShortestDistance(values);
+
+        shortestDistance.calculateDistance();
+        assertEquals(shortestDistance.getDistance(), Math.sqrt(2));
+    }
+
+    @Test
+    public void testDistanceCalculationWithMixedNumbers() {
+        double[] values = {-1, -1, -2, -2};
+        ShortestDistance shortestDistance = new ShortestDistance(values);
+
+        shortestDistance.calculateDistance();
+        assertEquals(shortestDistance.getDistance(), Math.sqrt(2));
+    }
+
+    @Test
+    public void testDistanceCalculationWithDecimalNumbers() {
+        double[] values = {1.5, 2, 6, 7.5};
+        ShortestDistance shortestDistance = new ShortestDistance(values);
+
+        shortestDistance.calculateDistance();
+        assertEquals(shortestDistance.getDistance(), Math.sqrt(50.5));
     }
 
 }

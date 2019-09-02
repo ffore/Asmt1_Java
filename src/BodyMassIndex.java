b81cbd0eb177b9d9dddeee2b492396178b3733d9
ffore <daniel.geleroff@gmail.com>
2019-09-02 16:30:09 -0400

    refactored height input validation

--- src/BodyMassIndex.java
+++ src/BodyMassIndex.java
@@ -25,35 +25,53 @@ public class BodyMassIndex {
     }
 
     public static boolean isValidHeight(String height){
-//      ' and " exits AND in correct order
         int singleQuoteIndex = height.indexOf('\'');
         int doubleQuoteIndex = height.indexOf('\"');
-        if(singleQuoteIndex == -1 || doubleQuoteIndex == -1 || doubleQuoteIndex < singleQuoteIndex){
-            return false;
-        }
 
+        if( !doQuotesExistInOrder(singleQuoteIndex, doubleQuoteIndex) ||
+            !doQuotesOccurOnce(height) ||
+            !isOnlyNumbersAndQuotes(height) ||
+            !areQuotesAfterNumbers(height, singleQuoteIndex, doubleQuoteIndex) ||
+            !areMeasurementsValid(height, singleQuoteIndex, doubleQuoteIndex)) return false;
+        else return true;
+    }
+
+    public static boolean doQuotesExistInOrder(int singleQuoteIndex, int doubleQuoteIndex){
+//      ' and " exits AND in correct order
+        if(singleQuoteIndex == -1 || doubleQuoteIndex == -1 || doubleQuoteIndex < singleQuoteIndex) return false;
+        else return true;
+    }
+
+    public static boolean doQuotesOccurOnce(String height){
 //      has one ' and one " only
 //      following code was inspired by: https://stackoverflow.com/a/8910767
         int singleCount = height.length() - height.replace("\'", "").length();
         int doubleCount = height.length() - height.replace("\"", "").length();
-        if(singleCount != 1 || doubleCount != 1){
-            return false;
-        }
+        if(singleCount != 1 || doubleCount != 1) return false;
+        else return true;
+    }
 
+    public static boolean isOnlyNumbersAndQuotes(String height){
 //      only numbers and ' and " are present (no decimals)
         String noQuoteHeight = height.replace("\'", "");
         noQuoteHeight = noQuoteHeight.replace("\"", "");
 //      following code was inspired by: https://stackoverflow.com/a/10575676
         if(!noQuoteHeight.matches("[0-9]+")) return false;
+        else return true;
+    }
 
+    public static boolean areQuotesAfterNumbers(String height, int singleQuoteIndex, int doubleQuoteIndex){
 //      ' and " are after numbers
         if( !height.substring(0, 1).matches("[0-9]+") ||
-            !height.substring(singleQuoteIndex-1, singleQuoteIndex).matches("[0-9]+") ||
-            !height.substring(singleQuoteIndex+1, singleQuoteIndex+2).matches("[0-9]+") ||
-            !height.substring(doubleQuoteIndex-1, doubleQuoteIndex).matches("[0-9]+") ||
-            height.length() != doubleQuoteIndex + 1
+                !height.substring(singleQuoteIndex-1, singleQuoteIndex).matches("[0-9]+") ||
+                !height.substring(singleQuoteIndex+1, singleQuoteIndex+2).matches("[0-9]+") ||
+                !height.substring(doubleQuoteIndex-1, doubleQuoteIndex).matches("[0-9]+") ||
+                height.length() != doubleQuoteIndex + 1
         ) return false;
+        else return true;
+    }
 
+    public static boolean areMeasurementsValid(String height, int singleQuoteIndex, int doubleQuoteIndex){
 //      height measurements are valid numbers
         String heightFtString = height.substring(0, singleQuoteIndex);
         String heightInString = height.substring(singleQuoteIndex+1, doubleQuoteIndex);
@@ -66,11 +84,8 @@ public class BodyMassIndex {
             return false;
         }
         if(heightIn > 11 || (heightFt == 0 && heightIn == 0)) return false;
-        return true;
+        else return true;
     }
 
-//    public static boolean doQuotesExistInOrder(String height){
-//
-//    }
 }
 

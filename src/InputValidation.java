b01a1c01cb05299220ed3f208aa8747799e7a709
ffore <daniel.geleroff@gmail.com>
2019-09-08 12:12:14 -0400

    bmi input validation now in separate class

--- src/InputValidation.java
+++ src/InputValidation.java
@@ -45,6 +45,75 @@ public class InputValidation {
         return true;
     }
 
+    public static boolean doQuotesExistInOrder(int singleQuoteIndex, int doubleQuoteIndex){
+//      ' and " exits AND in correct order
+        if(singleQuoteIndex == -1 || doubleQuoteIndex == -1 || doubleQuoteIndex < singleQuoteIndex) return false;
+        else return true;
+    }
+
+    public static boolean doQuotesOccurOnce(String height){
+//      has one ' and one " only
+//      following code was inspired by: https://stackoverflow.com/a/8910767
+        int singleCount = height.length() - height.replace("\'", "").length();
+        int doubleCount = height.length() - height.replace("\"", "").length();
+        if(singleCount != 1 || doubleCount != 1) return false;
+        else return true;
+    }
+
+    public static boolean isOnlyNumbersAndQuotes(String height){
+//      only numbers and ' and " are present (no decimals)
+        String noQuoteHeight = height.replace("\'", "");
+        noQuoteHeight = noQuoteHeight.replace("\"", "");
+//      following code was inspired by: https://stackoverflow.com/a/10575676
+        if(!noQuoteHeight.matches("[0-9]+")) return false;
+        else return true;
+    }
+
+    public static boolean areQuotesAfterNumbers(String height, int singleQuoteIndex, int doubleQuoteIndex){
+//      ' and " are after numbers
+        if( !height.substring(0, 1).matches("[0-9]+") ||
+                !height.substring(singleQuoteIndex-1, singleQuoteIndex).matches("[0-9]+") ||
+                !height.substring(singleQuoteIndex+1, singleQuoteIndex+2).matches("[0-9]+") ||
+                !height.substring(doubleQuoteIndex-1, doubleQuoteIndex).matches("[0-9]+") ||
+                height.length() != doubleQuoteIndex + 1
+        ) return false;
+        else return true;
+    }
+
+    public static boolean areNumbersZeroPreceding(String height, int singleQuoteIndex, int doubleQuoteIndex){
+        String ftString = height.substring(0,singleQuoteIndex);
+        String inString = height.substring(singleQuoteIndex+1, doubleQuoteIndex);
+        if( ftString.charAt(0) == '0' && ftString.length() != 1 ||
+                inString.charAt(0) == '0' && inString.length() != 1) return false;
+        return true;
+
+    }
+
+    public static boolean areMeasurementsValid(String height, int singleQuoteIndex, int doubleQuoteIndex){
+//      height measurements are valid numbers
+        String heightFtString = height.substring(0, singleQuoteIndex);
+        String heightInString = height.substring(singleQuoteIndex+1, doubleQuoteIndex);
+        int heightFt;
+        int heightIn;
+        try {
+            heightFt = Integer.parseInt(heightFtString);
+            heightIn = Integer.parseInt(heightInString);
+        } catch(NumberFormatException e) {
+            return false;
+        }
+        if(heightIn > 11 || (heightFt == 0 && heightIn == 0)) return false;
+        else return true;
+    }
+
+    public static boolean isOnlyNumbers(String weight){
+        if(!weight.matches("[0-9]+")) return false;
+        else return true;
+    }
+
+    public static boolean isFirstCharValidDigit(String weight){
+        if(weight.charAt(0) != '0') return true;
+        else return false;
+    }
 
 
 }

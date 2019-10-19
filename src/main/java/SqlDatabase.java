72a0f92284577c3da13b366c63e05249d64d7bf9
ffore <daniel.geleroff@gmail.com>
2019-10-18 20:03:15 -0400

    check if db exists, and makes tables accordingly

--- src/main/java/SqlDatabase.java
+++ src/main/java/SqlDatabase.java
@@ -5,33 +5,42 @@ public class SqlDatabase {
 
     public static void main(String[] args) {
         connectToDatabase();
+
     }
 
     public static void connectToDatabase() {
         try {
-            startConnection();
+            Connection connection = startConnection();
+            if(!ppa2_dbExists(connection)){
+                System.out.println("ppa2_db not found \ncreating one now...");
+                createPpa2_db(connection);
+                createDistanceTable(connection);
+                createBMITable(connection);
+            }
+            else System.out.println("pap2_db found");
         } catch (Exception e) {
             System.out.println(e);
+            return;
         }
     }
 
-    public static void startConnection() throws Exception {
-        String url = "jdbc:mysql://192.168.99.100:3306/testdb";
+    public static Connection startConnection() throws Exception {
+        String url = "jdbc:mysql://192.168.99.100:3306/";
         String user = "root";
-        String password = "password";
+        String password = "pass"; // probably gotta switch to 'password'
 
         loadDriver();
         Connection connection = establishConnection(url, user, password);
-        Statement statement = connection.createStatement();
-        ResultSet result = statement.executeQuery("SELECT * FROM example");
+        return connection;
+//        Statement statement = connection.createStatement();
+//        ResultSet result = statement.executeQuery("SELECT * FROM danny_test.people");
+//
+//        System.out.println("Results from Query...");
+//        result.next();
+//        System.out.println(result.getInt("age") + " " +
+//                result.getString("name"));
 
-        System.out.println("Results from Query...");
-        result.next();
-        System.out.println(result.getInt("id") + " " +
-                result.getString("name") + " " +
-                result.getInt("age"));
-
-        closeConnection(connection);
+//        closeConnection(connection);
     }
 
     public static void loadDriver() throws Exception {
@@ -45,6 +54,37 @@ public class SqlDatabase {
         Connection connection = DriverManager.getConnection(url, user, password);
         System.out.println("Database connected!");
         return connection;
+    }
+
+    public static boolean ppa2_dbExists(Connection connection) throws Exception {
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("SHOW DATABASES;");
+        Boolean dbExists = false;
+        while(rs.next()){
+            String db = rs.getString(1);
+            if(db.equals("ppa2_db")) dbExists = true;
+        }
+        return dbExists;
+    }
+
+    public static void createPpa2_db(Connection connection) throws Exception {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("CREATE DATABASE ppa2_db;");
+        System.out.println("Created ppa2_db");
+    }
+
+    public static void createDistanceTable(Connection connection) throws Exception {
+        Statement statement = connection.createStatement();
+        String stmt = "CREATE TABLE ppa2_db.Distance ( id INT AUTO_INCREMENT PRIMARY KEY, Timestamp VARCHAR(20), x1 DECIMAL(10,3), x2 DECIMAL(10,3), y1 DECIMAL(10,3), y2 DECIMAL(10,3), Result DECIMAL(10,3));";
+        statement.executeUpdate(stmt);
+        System.out.println("Created Distance table");
+    }
+
+    public static void createBMITable(Connection connection) throws Exception {
+        Statement statement = connection.createStatement();
+        String stmt = "CREATE TABLE ppa2_db.Bmi ( id INT AUTO_INCREMENT PRIMARY KEY, Timestamp VARCHAR(20), Height VARCHAR(10), Weight INT, Result DECIMAL(10,3));";
+        statement.executeUpdate(stmt);
+        System.out.println("Created Bmi table");
     }
 
     public static void closeConnection(Connection connection) throws Exception {
